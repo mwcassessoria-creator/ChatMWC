3c06f078ed6cf47331b1c0aa5ee09e273501370b
Anderson <anderson.h08@gmail.com>
2026-01-21 20:50:09 -0300

    fix(render): Remove executablePath to use Puppeteer bundled Chromium

--- server.js
+++ server.js
@@ -34,7 +34,6 @@ const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
         headless: true,
-        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || undefined,
         args: [
             "--no-sandbox",
             "--disable-setuid-sandbox",
